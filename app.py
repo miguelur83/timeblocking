dc7576528a56e641bfa5e63931e9364dcca56c44
Miguel Urdinola <miguelur@gmail.com>
2024-08-02 19:18:48 -0500

    app.py update entry point

--- app.py
+++ app.py
@@ -78,5 +78,5 @@ def generate():# Extract available days
         error_message = str(e)
         return render_template('results.html', error=error_message)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
